4f1b8b963533b1432d5859c964b7ca01da3e3be6
Inayat-khan <khanetmoi@gmail.com>
2023-07-19 19:39:04 +0000

    sponsored component

--- afrowatch/src/components/Ho/Sponsored.js
+++ afrowatch/src/components/Ho/Sponsored.js
@@ -3,29 +3,47 @@ import styled from 'styled-components';
 
 const Sponsor = styled.div`
 display: flex;
-justify-content: center;
-align-items: center;
-height: 18rem;
-margin-block: 1rem;
-
-  video {
+height: 25rem;
+width: 100%;
+background-color:rgba(118,184,82,1);
+  img {
+    width:33%;
     height: 100%;
-    width: 100%;
-    border: 2px solid black;
+    border: none;
+  }
+  .above {
+    color: white;
+    height: 100%;
+    width: 33%;
+    border-right: none;
+    background-color: rgba(146,104,76,0.9);
   }
 `;
 
-const Sponsored = ({picture, trailer, title, year})=>{
-
+const Sponsored = ()=>{
+    const sponsoredMovie = { trailer: "https://app.sipconsult.net/afrowatch/EXPEND4BLES%20(2023)%20Official%20Trailer%20-%20Jason%20Statham,%2050%20Cent,%20Megan%20Fox,%20Dolph%20Lundgren.mp4",
+    picture: "/workspaces/Afrawatch_client/afrowatch/src/mandala.png",
+    title: "title4",
+    year : 2011,
+    genre: "action",
+    description: "lorem ipsum",
+    id: 4
+  }
     return (
-        <Sponsor>
-
-        <video controls>
-          <source src={trailer} type="video/mp4" />
-        </video>
+    <Sponsor>
+      <img src={Sponsored.picture} alt="sponsored movie"/>
+      <div className='above'>
+      <h2>title: {sponsoredMovie.title}</h2>
+      <p>description: {sponsoredMovie.description}</p>
+      <span>genre : {sponsoredMovie.genre}</span>
+      <span>year: {sponsoredMovie.year}</span>
+      <button>Play</button>
+      <button>Info</button>
+      </div>
+      <video>
+      <source src={sponsoredMovie.trailer} type="video/mp4" />
+      </video>
       
-      <h2>{title}</h2>
-      <h3>{year}</h3>
     </Sponsor >
     )
 }
